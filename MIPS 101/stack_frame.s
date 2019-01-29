e230d0d91d08c38461fd748128affa67b4734cbf
Hielo <juanfgarciacas@gmail.com>
2019-01-29 19:07:00 +0100

    Update stack_frame.s

--- MIPS 101/stack_frame.s
+++ MIPS 101/stack_frame.s
@@ -11,11 +11,11 @@
 
 main:
 
-	li $a0 2	
+	li $a0 2
 	li $a1 5
 	li $a2 8
 	li $a3 12
-	
+
 	#Push
 	addu $sp $sp -8
 	li $t0 8
@@ -23,7 +23,8 @@ main:
 	li $t0 4
 	sw $t0 4($sp)
 	jal f
-	#Pop
+
+
 
 f:	# v[i] = n1 + n2 + n3 + n4 + n5 + n6
 
@@ -35,34 +36,31 @@ f:	# v[i] = n1 + n2 + n3 + n4 + n5 + n6
 
 	#Leave space for the int[4] array
 	addu $sp $sp -16
-	
-	#Calculate n1 + n2 + n3 + n4 + n5 + n6
-	add $t0 $a0 $a1
-	add $t0 $t0 $a2
-	add $t0 $t0 $a3
-	lw $t1 4($fp)
-	add $t0 $t0 $t1
-	lw $t1 8($fp)
-	add $t0 $t0 $t1
 
-	li $t1, 0
-	li $t3, 3
-	
+	#Calculate n1 + n2 + n3 + n4 + n5 + n6
+	add $t0 $a0 $a1 	#t0 = n1 + n2
+	add $t0 $t0 $a2		#t0 = t0 + n3
+	add $t0 $t0 $a3		#t0 = t0 + n4
+	lw $t1 4($fp)		#Bring n5 from the fp
+	add $t0 $t0 $t1		#t0 = t0 + n5
+	lw $t1 8($fp)		#Bring n6 from the fp
+	add $t0 $t0 $t1		#t0 = t0 +n6
+
+	li $t1  0 	#index for loop
+	li $t3  3	#array lenght
+
+	#Copy the array initial direction from fp
+	#This is needed because we can't manipulate fp value
 	move $t2 $fp
 	addi $t2 $t2 -16
-	li $t3 3
 
-bucle: bgt $t1 $t3 fin
-	sw $t0 ($t2)
-	addi $t0 $t0 4
-	addi $t1 $t1 1
-	b bucle
-fin:
-	lw $v0 -12($fp)
-	lw $fp ($fp)
-	addu $sp $sp 20
+loop:   bgt $t1 $t3 end 	# if (index = lenght) jump to end
+	sw $t0 ($t2)		# v[i] = t0 (were sum of all nubers it's allocated)
+	addi $t2 $t2 4		# t2 + 4 for going to next array element
+	addi $t1 $t1 1		# index++
+	b loop			# jump to lopp
+end:
+	lw $v0 -12($fp)		# Return the direction of first elemnt
+	lw $fp ($fp)		# Restore the fp value
+	addu $sp $sp 20		# Restore the sp position
 	jr $ra
-	
-	
-
-
